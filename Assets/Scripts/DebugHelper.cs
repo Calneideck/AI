ec59d4ca02aa43ad9eefc9d4948ebb06530f5687
Callum Neideck <cal_neideck@hotmail.com>
2017-05-20 19:13:28 +1000

    MOD

---
+++ Assets/Scripts/DebugHelper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+
+public class DebugHelper : MonoBehaviour
+{
+    [SerializeField]
+    private bool _paths;
+    [SerializeField]
+    private bool _sightRanges;
+
+    public static bool paths;
+    public static bool sightPaths;
+
+    void OnValidate()
+    {
+        paths = _paths;
+        sightPaths = _sightRanges;
+    }
+}
--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -1,27 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Guard : MonoBehaviour
 {
+    private enum State { PATROLLING, ENGAGING, RELOADING, ALERTED };
+    private State state = State.PATROLLING;
+
     private Vector3[] path;
     private int pathIndex;
     private bool requestingPath = true;
+    private Player player;
+
+    public float detection = 0;
 
     public float speed = 4;
+    public float minSightRange;
+    public float maxSightRange;
+    public Image detectionImage;
+    public LayerMask wallMask;
 
     void Start()
     {
         Pathfinding.instance.RequestPath(gameObject, transform.position);
+        player = GameObject.Find("Player").GetComponent<Player>();
+        minSightRange *= minSightRange;
+        maxSightRange *= maxSightRange;
     }
 
     void Update()
+    {
+        switch (state)
+        {
+            case State.PATROLLING:
+                Move();
+                break;
+            case State.ENGAGING:
+
+                break;
+            case State.ALERTED:
+
+                break;
+            case State.RELOADING:
+
+
+                break;
+        }
+
+        DetectPlayer();
+        detectionImage.transform.parent.rotation = Quaternion.Euler(Vector3.right * 90);
+    }
+
+    void DetectPlayer()
+    {
+        // Check if has line of sight
+        RaycastHit wallHit;
+        if (Physics.Raycast(transform.position, player.transform.position - transform.position, out wallHit, 100, wallMask))
+            if (wallHit.collider != null)
+            {
+                detection = 1 - Mathf.InverseLerp(minSightRange, maxSightRange, (player.transform.position - transform.position).sqrMagnitude);
+
+                float los = 1;
+                if (Vector3.Angle(player.transform.position - transform.position, transform.forward) < 60)
+                    los = 1.4f;
+
+                detection *= los;
+            }
+
+        detectionImage.fillAmount = detection;
+    }
+
+    void Move()
     {
         if (requestingPath)
             return;
 
         Vector3 target = path[pathIndex];
-        target.y = 0.6f;
+        target.y = 0.3f;
         transform.Translate((target - transform.position).normalized * speed * Time.deltaTime, Space.World);
         transform.LookAt(target);
 
@@ -51,11 +107,18 @@ public class Guard : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        if (path != null)
+        if (DebugHelper.paths && path != null)
         {
             Gizmos.color = Color.red;
             for (int i = 1; i < path.Length; i++)
                 Gizmos.DrawLine(path[i - 1], path[i]);
+        }
+
+        if (DebugHelper.sightPaths)
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireSphere(transform.position, Application.isPlaying ? Mathf.Sqrt(minSightRange) : minSightRange);
+            Gizmos.DrawWireSphere(transform.position, Application.isPlaying ? Mathf.Sqrt(maxSightRange) : maxSightRange);
         }
     }
 }
